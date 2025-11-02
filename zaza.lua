7da91d806c4c273aded8096d4a5c989142271326
cloudhub4416tuff <cloudhub4416@gmail.com>
2025-11-02 18:01:43 +0100

    Create zaza.lua

---
+++ zaza.lua
@@ -0,0 +1,6 @@
+local Players = game:GetService("Players")
+local client = Players.LocalPlayer
+
+local TeleportService = game:GetService("TeleportService")
+
+TeleportService:Teleport(13883279773, client)
